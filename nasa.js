415e396bc63003dcb0f294fbbbaa4d93960d274c
Gabe <USER@Gabes-MacBook-Air.local>
2016-06-02 13:28:54 -0700

    multiple svgs update

--- nasa.js
+++ nasa.js
@@ -47,44 +47,8 @@ d3.csv("nasa.csv", function(error, data) {
     x.domain(d3.extent(data, function(d) { return d.date; }));
     y.domain([0, d3.max(data, function(d) { return d.share; })]);
 
-    /*
-     svg.append("linearGradient")				
-        .attr("id", "line-gradient")			
-        .attr("gradientUnits", "userSpaceOnUse")	
-        .attr("x1", 0).attr("y1", y(0))			
-        .attr("x2", 0).attr("y2", y(1000))		
-      .selectAll("stop")						
-        .data([								
-            {offset: "0%", color: "red"},		
-            {offset: "40%", color: "red"},	
-            {offset: "40%", color: "black"},		
-            {offset: "62%", color: "black"},		
-            {offset: "62%", color: "lawngreen"},	
-            {offset: "100%", color: "lawngreen"}	
-        ])					
-    .enter().append("stop")			
-        .attr("offset", function(d) { return d.offset; })	
-        .attr("stop-color", function(d) { return d.color; });
-    */
-    /*var lineGraph1 = svg.append("path")
-        .attr("d", valueline(data.filter(function(d) {
-            return d.party == 'blue';
-        })))
-        .attr("stroke", "blue")
-        .attr("stroke-width", 2)
-        .attr("fill", "none");
-    
-    
-    var lineGraph2 = svg.append("path")
-        .attr("d", valueline(data.filter(function(d) {
-            return d.party == 'red';
-        })))"
-        .attr("stroke", "red")
-        .attr("stroke-width", 2)
-        .attr("fill", "none");
-    
-*/
-    var color = d3.scale.ordinal() .domain(["1985","1986","1987","1988","1989","1990","1991","1992","1993","1994","1995","1996","1997","1998","1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015"]) .range("#FF0000","#FF0000","#FF0000","#FF0000","#FF0000","#FF0000","#FF0000","#FF0000",      "#0000FF","#0000FF","#0000FF","#0000FF","#0000FF","#0000FF","#0000FF","#0000FF",      "#FF0000","#FF0000","#FF0000","#FF0000","#FF0000","#FF0000","#FF0000","#FF0000", "#0000FF","#0000FF","#0000FF","#0000FF","#0000FF","#0000FF","#0000FF","#0000FF");
+
+   // var color = d3.scale.ordinal() .domain(["1985","1986","1987","1988","1989","1990","1991","1992","1993","1994","1995","1996","1997","1998","1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015"]) .range("#FF0000","#FF0000","#FF0000","#FF0000","#FF0000","#FF0000","#FF0000","#FF0000",      "#0000FF","#0000FF","#0000FF","#0000FF","#0000FF","#0000FF","#0000FF","#0000FF",      "#FF0000","#FF0000","#FF0000","#FF0000","#FF0000","#FF0000","#FF0000","#FF0000", "#0000FF","#0000FF","#0000FF","#0000FF","#0000FF","#0000FF","#0000FF","#0000FF");
     
     
     // Add the valueline path.
